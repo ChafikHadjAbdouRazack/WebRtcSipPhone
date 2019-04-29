9ae8e2101d434b231777a19da7b23bfa52d9016d
ChafikHadjAbdouRazack <yazassr@gmail.com>
2019-04-29 16:00:01 +0100

    Ajout de fonctionnalité

--- src/js/app.js
+++ src/js/app.js
@@ -4,37 +4,37 @@ $(function () {
     $('#login-btn, #server-address, #name, #exten, #password').attr('disabled','disabled');
     $('#logout-btn,#call-to,#call-btn').removeAttr('disabled');
     $('#call-to').focus();
-    $('#output-lbl').text('Registered');
+    $('#output-lbl').text('Connecté');
   });
 
   $(document).on('unregistered', function (ev) {
     $('#login-btn, #server-address, #name, #exten, #password').removeAttr('disabled');
     $('#logout-btn,#call-to,#call-btn').attr('disabled','disabled');
-    $('#output-lbl').text('Unregistered');
+    $('#output-lbl').text('Déconnecté');
   });
 
   $(document).on('calling', function (ev) {
     $('#hangup-btn').removeAttr('disabled');
     $('#call-btn,#call-to').attr('disabled','disabled');
-    $('#output-lbl').text('Calling ' + webrtcPhone.getCounterpartNum() + '...');
+    $('#output-lbl').text('Appel ' + webrtcPhone.getCounterpartNum() + '...');
   });
 
   $(document).on('incomingcall', function (ev, from) {
     $('#hangup-btn,#answer-btn').removeAttr('disabled');
     $('#call-btn,#call-to').attr('disabled','disabled');
-    $('#output-lbl').text('...incoming call from ' + from);
+    $('#output-lbl').text('... appel entrant de ' + from);
   });
 
   $(document).on('callaccepted', function (ev) {
     $('#hangup-btn').removeAttr('disabled');
     $('#call-btn,#call-to,#answer-btn').attr('disabled','disabled');
-    $('#output-lbl').text('...in conversation with ' + webrtcPhone.getCounterpartNum());
+    $('#output-lbl').text('... en conversation avec ' + webrtcPhone.getCounterpartNum());
   });
 
   $(document).on('hangup', function (ev) {
     $('#hangup-btn,#answer-btn').attr('disabled','disabled');
     $('#call-btn,#call-to').removeAttr('disabled');
-    $('#output-lbl').text('Registered');
+    $('#output-lbl').text('Connecté');
   });
 
   $('#login-btn').click(function () {
